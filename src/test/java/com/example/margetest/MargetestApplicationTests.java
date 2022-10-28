4b3500c516000e79a1733613c375240a9050e7cc
toshio-nakayama <toshio.h1990@gmail.com>
2022-10-28 17:16:58 +0900

    Initial commit

---
+++ src/test/java/com/example/margetest/MargetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.margetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MargetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
